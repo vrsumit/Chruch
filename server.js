5cb4697b7b48f58648f2b17e0676c8f2c8873855
vrsumit <sahane.sumit35@gmail.com>
2020-02-17 19:34:54 +0530

    Changed configuration server

--- server.js
+++ server.js
@@ -1,9 +1,14 @@
 // server.js
 const express = require('express');
+
+const path = require('path');
 const app = express();
 // Run the app by serving the static files
 // in the dist directory
-app.use(express.static(__dirname + '/dist'));
+//app.use(express.static(__dirname + '/dist/Church'));
+app.get('/*', function(req, res) {
+    res.sendFile(path.join(__dirname + '/dist/Church/index.html'));
+  });
 // Start the app by listening on the default
 // Heroku port
 app.listen(process.env.PORT || 8080);
